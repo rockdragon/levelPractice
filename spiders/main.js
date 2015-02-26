28fb30f84d68848ab3f9cee3a0b0c036f99a36bf
rockdragon <moyerock@gmail.com>
2015-02-26 16:45:40 +0800

    fix output.

--- spiders/main.js
+++ spiders/main.js
@@ -1,9 +1,16 @@
+var util = require('util');
+var fs = require('fs');
 var spider = require('./spider');
 var pageURL = 'http://item.jd.com/1184730.html';
 
 spider.crawlPage(pageURL, function(err, res){
     if(err)
         console.log('caught error:\n', err);
-    else
-        console.log(res);
+    else {
+        console.log('END ----------------->>>>');
+        console.log(util.inspect(res));
+
+        fs.writeFileSync('result.json', res);
+
+    }
 });
--- spiders/spider.js
+++ spiders/spider.js
@@ -1,7 +1,8 @@
 var cheerio = require('cheerio');
+var util = require('util');
 
 var By = require('selenium-webdriver').By,
-    until = require('selenium-webdriver').until,
+    //until = require('selenium-webdriver').until,
 //browser = require('selenium-webdriver/firefox');
     browser = require('selenium-webdriver/chrome');
 var driver = new browser.Driver();
@@ -19,35 +20,39 @@ function crawlPage(pageURL, cb) {
 
 function extractComments(cb) {
     driver.findElement(By.className('com-table-main')).getInnerHtml().then(function (innerHTML) {
-        var $ = cheerio.load(innerHTML, {
-            normalizeWhitespace: true,
-            xmlMode: true
-        });
-        $('table.com-item-main tr').each(function () {
-            var columns = $(this).children();
-            var k = $(columns[2]).find('span.text')[0].text();
-            var star = parseInt($(columns[1]).find('div.grade-star')[0].attr('class').substr(-1), 10);
-            meta[k] = meta.hasOwnProperty(k) ? meta[k] + 1 : 1;
-            meta[k][star] = meta[k].hasOwnProperty(star) ? meta[k][star] + 1 : 1;
-            console.log(k, meta[k]);
+        driver.findElement(By.className('ui-page-curr')).getInnerHtml().then(function(currentPage){
+            console.log(util.format('\n当前页:%s ----------------->>>>', currentPage));
 
-            process.nextTick(function () {
-                var next = driver.findElement(By.className('ui-pager-next'));
-                if (next) {
-                    next.click().then(function () {
-                        driver.sleep(2000).then(function () {
-                            extractComments(cb);
+            var $ = cheerio.load(innerHTML, {
+                normalizeWhitespace: true,
+                xmlMode: true
+            });
+            $('table.com-item-main tr').each(function () {
+                var columns = $(this).children();
+                var k = $(columns[2]).children().first().children().last().text();
+                var star = $(columns[1]).children().first().attr('class').substr(-1) + '星';
+                meta[k] = meta.hasOwnProperty(k) ? meta[k] + 1 : 1;
+                meta[k][star] = meta[k].hasOwnProperty(star) ? meta[k][star] + 1 : 1;
+                console.log(util.inspect(meta));
+
+                process.nextTick(function () {
+                    var next = driver.findElement(By.className('ui-pager-next'));
+                    if (next) {
+                        next.click().then(function () {
+                            driver.sleep(2000).then(function () {
+                                extractComments(cb);
+                            });
                         });
-                    });
-                } else {
-                    return cb(null, meta);
-                }
+                    } else {
+                        return cb(null, meta);
+                    }
+                });
             });
         });
     })
-        .then(null, function (err) {
-            cb(err, null);
-        });
+    .then(null, function (err) {
+        cb(err, null);
+    });
 }
 
 module.exports.crawlPage = crawlPage;
